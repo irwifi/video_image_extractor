1a6c930de7a8a833ed0dccc41720f973caec5e77
Raju Malla <irwifi@gmail.com>
2017-08-05 18:38:11 +0545

    Fixed for iphone.

--- index.js
+++ index.js
@@ -2,10 +2,28 @@
 $(function() {
     var formdata = new FormData();
     var URL = window.URL || window.webkitURL
+    var video = document.createElement("video");
+    var thumbs = document.getElementById("thumbs");
 
     document.querySelector('input[type="file"]').addEventListener('change', function(e) { select_video(this); }, false);
 
     $("#start").on("click", playSelectedFile);
+
+    video.addEventListener('loadeddata', function() {
+        thumbs.innerHTML = "";
+        video.currentTime = 0;
+    }, false);
+
+    video.addEventListener('seeked', function() {
+        // now video has seeked and current frames will show at the time as we expect
+        generateThumbnail();
+
+        // if we are not passed end, seek to next interval
+        if (video.currentTime + 1 <= video.duration) {
+            // this will trigger another seeked event
+            video.currentTime += 1;
+        }
+    }, false);
 
     function select_video(file_obj) {
         var formData = new FormData();
@@ -45,56 +63,21 @@ $(function() {
         $("#thumbs").show();
 
         var fileURL = URL.createObjectURL(document.querySelector('input[type="file"]').files[0])
-        var i = 0;
-        var video = document.createElement("video");
-        var thumbs = document.getElementById("thumbs");
-
+        video.autoplay = true;
+        video.muted = true;
         video.preload = "auto";
+        video.currentTime = 0;
         video.src = fileURL;
-        video.currentTime = i;
+    }
 
-        video.addEventListener('loadeddata', function() {
-            thumbs.innerHTML = "";
-            video.currentTime = i;
-        }, false);
+    function generateThumbnail() {
+        var c = document.createElement("canvas");
+        c.id = "canvas";
 
-        video.addEventListener('seeked', function() {
-            // now video has seeked and current frames will show at the time as we expect
-            generateThumbnail(i);
-
-            // when frame is captured, increase
-            i++;
-
-            // if we are not passed end, seek to next interval
-            if (i <= video.duration) {
-                // this will trigger another seeked event
-                video.currentTime = i;
-            }
-        }, false);
-
-
-         function generateThumbnail(j) {
-            var c = document.createElement("canvas");
-            c.id = "canvas";
-
-            var ctx = c.getContext("2d");
-            c.width = 160;
-            c.height = 90;
-            ctx.drawImage(video, 0, 0, 160, 90);
-            thumbs.appendChild(c);
-
-            var imageData = c.toDataURL("image/png");
-            var blobBin = atob(imageData.split(',')[1]);
-            var array = [];
-            for (var i = 0; i < blobBin.length; i++) {
-                array.push(blobBin.charCodeAt(i));
-            }
-            var file = new Blob([new Uint8Array(array)], { type: 'image/png' });
-            formdata.append("image[]", file, "abc" + thumbs.childElementCount);
-
-            for (var pair of formdata.entries()) {
-                console.log(pair[0] + ', ' + pair[1]);
-            }
-        }
+        var ctx = c.getContext("2d");
+        c.width = 160;
+        c.height = 90;
+        ctx.drawImage(video, 0, 0, 160, 90);
+        thumbs.appendChild(c);
     }
 });
